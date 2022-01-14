780cfd7acabdb6c8017eb93badee4e5ce2d6185c
shaheer <shaheer@proppioneers.com>
2022-01-14 17:29:59 +0500

    template changes

--- ppBackend/__init__.py
+++ ppBackend/__init__.py
@@ -1,6 +1,8 @@
 # Framework imports
 from flask import Flask
 from flask import render_template
+from flask_moment import Moment
+from datetime import datetime
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 # Local imports
@@ -13,6 +15,7 @@ app = Flask(__name__)
 CORS(app)
 app.config["MONGODB_HOST"] = config.MONGO_DB_URI
 initialize_db(app)
+moment = Moment(app)
 bcrypt = Bcrypt(app)
 app.config.update(config.MAIL_SETTINGS)
 from ppBackend.scripts import run_scripts
