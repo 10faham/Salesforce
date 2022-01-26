cd3393a366cb6a715f0c3261c48e1989d202060d
Shaheer Shah <cs_1014@hotmail.com>
2022-01-26 13:03:00 +0500

    followups changes

--- ppBackend/static/pages/jquery.datatables.init.js
+++ ppBackend/static/pages/jquery.datatables.init.js
@@ -6,24 +6,26 @@
  */
 var handleDataTableButtons = function() {
         "use strict";
+        // $('#datatable').DataTable();
         $("table").each(function(){
-            0 !== $(this).length && $(this).DataTable({
-                dom: "Bfrtip",
-                buttons: [{
-                    extend: "copy",
-                    className: "btn-sm"
-                }, {
-                    extend: "excel",
-                    className: "btn-sm"
-                }, {
-                    extend: "pdf",
-                    className: "btn-sm"
-                }, {
-                    extend: "print",
-                    className: "btn-sm"
-                }],
-                responsive: !0
-            })
+            $(this).DataTable();
+            // 0 !== $(this).length && $(this).DataTable({
+            //     dom: "Bfrtip",
+            //     buttons: [{
+            //         extend: "copy",
+            //         className: "btn-sm"
+            //     }, {
+            //         extend: "excel",
+            //         className: "btn-sm"
+            //     }, {
+            //         extend: "pdf",
+            //         className: "btn-sm"
+            //     }, {
+            //         extend: "print",
+            //         className: "btn-sm"
+            //     }],
+            //     responsive: !0
+            // })
         })
     },
     TableManageButtons = function() {
