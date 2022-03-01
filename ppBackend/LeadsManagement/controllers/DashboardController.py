e2d6de5741481cfa23739717cb8556ed682e8b49
Shaheer Shah <cs_1014@hotmail.com>
2022-03-01 11:54:57 +0500

    KPI changes

--- ppBackend/LeadsManagement/controllers/DashboardController.py
+++ ppBackend/LeadsManagement/controllers/DashboardController.py
@@ -20,12 +20,17 @@ class DashboardController(Controller):
         user_childs = UserController.get_user_childs(
             user=common_utils.current_user(), return_self=True)
         lead_dataset = []
-        # queryset = cls.db_read_records(read_filter={constants.CREATED_BY: user, **data})
+        temp = []
         for user in user_childs:
-            queryset = cls.db_read_records(read_filter={
-                constants.CREATED_BY: user, **data})
-            lead_dataset.append([user.name, len(queryset)])
-        # lead_dataset.append(len(queryset))
+            if user == common_utils.current_user():
+                queryset = cls.db_read_records(read_filter={constants.CREATED_BY: user, **data})
+                lead_dataset.append([user.name, len(queryset)])
+            else:
+                queryset = cls.db_read_records(read_filter={constants.CREATED_BY: user, **data})
+                temp.append([user.name, len(queryset)])
+        for each in temp:
+            lead_dataset.append(each)
+
         return response_utils.get_response_object(
             response_code=response_codes.CODE_SUCCESS,
             response_message=response_codes.MESSAGE_SUCCESS,
@@ -56,20 +61,32 @@ class DashboardFollow(Controller):
         queryset = cls.db_read_records(read_filter={constants.CREATED_BY: user, **data})
         user_childs = UserController.get_user_childs(
             user=common_utils.current_user(), return_self=True)
+        temp = []
         for user in user_childs:
-            queryset = cls.db_read_records(read_filter={
-                constants.CREATED_BY: user, **data})
-            follow_dataset.append([user.name, [obj.display()
-                                for obj in queryset]])
+            if user == common_utils.current_user():
+                queryset = cls.db_read_records(read_filter={constants.CREATED_BY: user, **data})
+                follow_dataset.append([user.name, [obj.display() for obj in queryset]])
+            else:
+                queryset = cls.db_read_records(read_filter={constants.CREATED_BY: user, **data})
+                temp.append([user.name, [obj.display() for obj in queryset]])
+        for each in temp:
+            follow_dataset.append(each)
+        
         for user in follow_dataset:
             calls = 0
             meetings = 0
+            at_calls = 0
+            v_calls = 0
             for follow in user[1]:
                 if follow['type'] == 'Call':
                     calls += 1
                 if follow['type'] == 'Meeting':
                     meetings += 1
-            kpi_dataset.append([user[0], calls, meetings])
+                if follow['sub_type'] == 'Contacted_client' or "Followed_up" or "Whatsapp_call" or "Meeting_Confirmed" or 'Meeting_cancelled' or "Meeting_postponed":
+                    v_calls += 1
+                if follow['sub_type'] == 'Call_attempt':
+                    at_calls += 1
+            kpi_dataset.append([user[0], calls, meetings, len(user[1]), at_calls, v_calls])
         return response_utils.get_response_object(
             response_code=response_codes.CODE_SUCCESS,
             response_message=response_codes.MESSAGE_SUCCESS,
